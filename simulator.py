b7778911e4dc738414dd51a0858f16671a2b396b
Kacper Kipa <51452942+Kicper@users.noreply.github.com>
2022-06-16 14:49:18 +0200

    Add files via upload

--- simulator.py
+++ simulator.py
@@ -1,8 +1,17 @@
-import psycopg2
+import pyodbc
 import argparse
 import sys
 from datetime import datetime, timedelta
 import random
+import warnings
+
+
+DRIVER_NAME = 'ODBC Driver 17 for SQL Server'
+SERVER_NAME = '(LocalDb)\LocalDB'
+DATABASE_NAME = 'Symulator'
+
+
+warnings.filterwarnings("ignore")
 
 
 def random_date(start, end):
@@ -20,37 +29,35 @@ parser.add_argument("-u", "--logins_list", nargs='+', help="Name of login")
 parser.add_argument("-i", "--items_list", nargs='+', help="Name of item")
 parser.add_argument("-b", "--beginning_date",
                     help="Date of beginning", type=datetime.fromisoformat, default=datetime.now())
-parser.add_argument("-e", "--ending_date", help="Date of ending",
-                    type=datetime.fromisoformat, default=datetime.now() + timedelta(days=5))
+parser.add_argument("-c", "--average_cost",
+                    help="Average beginning cost of item", type=int, default=50)
+parser.add_argument("-f", "--not_finished", help="Auction is not finished",
+                    action=argparse.BooleanOptionalAction)
 args = parser.parse_args()
 print(args.number_of_records, args.logins_list,
-      args.items_list, args.beginning_date, args.ending_date)
+      args.items_list, args.beginning_date, args.average_cost, args.not_finished)
 
 
 # connect to the db and create the cursor
-con = psycopg2.connect(
-    host="localhost",
-    database="simulator_v1",
-    user="postgres",
-    password="66kipulet",
-    port="5433"
-)
+connectionString = f"""
+    Driver={{{DRIVER_NAME}}};
+    Server={SERVER_NAME};
+    Database={DATABASE_NAME};
+    Trusted_Connection=yes;
+"""
+
+con = pyodbc.connect(connectionString)
 cur = con.cursor()
 
 
 # check if entered values are correct
+beginning_date = args.beginning_date
+not_finished = args.not_finished
 if args.number_of_records <= 0:
     print("The number of records is incorrect!")
     sys.exit()
 else:
     number_of_records = args.number_of_records
-
-if args.ending_date - args.beginning_date <= timedelta(seconds=0):
-    print("The date range is incorrect!")
-    sys.exit()
-else:
-    ending_date = args.ending_date
-    beginning_date = args.beginning_date
 
 if not args.logins_list:
     cur.execute("SELECT person_id, login FROM person")
@@ -83,20 +90,28 @@ else:
         print("The list of items contains nonexisting item name!")
         sys.exit()
 
+if args.average_cost < 0:
+    print("The average cost is incorrect!")
+    sys.exit()
+else:
+    average_cost = args.average_cost
+
 
 # create auctions
 print(logins_list)
 print(items_list)
 for i in range(number_of_records):
     item = random.randint(0, len(items_list) - 1)
-    sql = "INSERT INTO auction_history (item_id) VALUES (%s) RETURNING auction_history_id;"
-    data = (str(items_list[item][0]),)
-    cur.execute(sql, (data))
+    sql = """INSERT INTO dbo.auction_history (item_id) VALUES (?)"""
+    data = (str(items_list[item][0]))
+    cur.execute(sql, data)
+    cur.execute("SELECT @@IDENTITY AS ID;")
     auction_history_id = cur.fetchone()[0]
+    # print(auction_history_id)
     con.commit()
 
-    number_of_offer = random.randint(10, 20)
-    price = random.randint(50, 100)
+    number_of_offer = random.randint(7, 23)
+    price = average_cost + random.randint(1, 50)
     next_date = random_date(
         beginning_date, beginning_date + timedelta(hours=5))
 
@@ -111,10 +126,11 @@ for i in range(number_of_records):
     print(temp_logins_list)
 
     for offer_iter in range(number_of_offer):
-        sql = "INSERT INTO offer (date, price, status, person_id, auction_history_id) VALUES (%s,%s,%s,%s,%s) RETURNING offer_id;"
-        data = (str(next_date), str(
-            price), "not_finished", str(person_id), str(auction_history_id),)
-        cur.execute(sql, (data))
+        sql = """INSERT INTO dbo.offer (date, price, status, person_id, auction_history_id) VALUES (?,?,?,?,?)"""
+        data = (next_date, str(
+            price), "not_finished", str(person_id), str(auction_history_id))
+        cur.execute(sql, data)
+        cur.execute("SELECT @@IDENTITY AS ID;")
 
         price += random.randint(1, 10)
         next_date = random_date(
@@ -128,10 +144,16 @@ for i in range(number_of_records):
 
         person_id = str(temp_logins_list[login][0])
 
-    sql = "INSERT INTO offer (date, price, status, person_id, auction_history_id) VALUES (%s,%s,%s,%s,%s) RETURNING offer_id;"
-    data = (str(next_date), str(
-        price), "finished", str(person_id), str(auction_history_id),)
-    cur.execute(sql, (data))
+    if(not_finished):
+        sql = """INSERT INTO dbo.offer (date, price, status, person_id, auction_history_id) VALUES (?,?,?,?,?)"""
+        data = (next_date, str(
+            price), "not_finished", str(person_id), str(auction_history_id))
+        cur.execute(sql, data)
+    else:
+        sql = """INSERT INTO dbo.offer (date, price, status, person_id, auction_history_id) VALUES (?,?,?,?,?)"""
+        data = (next_date, str(
+            price), "finished", str(person_id), str(auction_history_id))
+        cur.execute(sql, data)
 
 
 con.commit()
